e5e4d82a2c5e162f694812b5a754c9208526b410
aklam <aklam@princeton.edu>
2018-12-25 20:16:16 -0800

    re-adding larger layers

--- Cornell_model.py
+++ Cornell_model.py
@@ -100,12 +100,12 @@ params['DECODER_RNN_TYPE'] = 'GRU'
 
 #params['N_LAYERS_ENCODER'] = 2
 #params['N_LAYERS_DECODER'] = 2
-#params['ENCODER_HIDDEN_SIZE'] = 512
-#params['DECODER_HIDDEN_SIZE'] = 512
-#params['MODEL_SIZE'] = 512
+params['ENCODER_HIDDEN_SIZE'] = 512
+params['DECODER_HIDDEN_SIZE'] = 512
+params['MODEL_SIZE'] = 512
 
-#params['SKIP_VECTORS_HIDDEN_SIZE'] = 512
-#params['ATTENTION_SIZE'] = 512
+params['SKIP_VECTORS_HIDDEN_SIZE'] = 512
+params['ATTENTION_SIZE'] = 512
 
 nmt_model = TranslationModel(params, 
 	model_type='GroundHogModel',
