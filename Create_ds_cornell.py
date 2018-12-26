3eab28061783144830f7ef66a241d1bdae6401ad
aklam <aklam@princeton.edu>
2018-12-26 15:46:19 -0800

    Added tokenization

--- Create_ds_cornell.py
+++ Create_ds_cornell.py
@@ -1,19 +1,21 @@
 import sys
 sys.path.append('../nmt-keras')
+sys.path.append('../nmt-keras/nmt_keras')
+
 from data_engine.prepare_data import keep_n_captions
 from keras_wrapper.dataset import Dataset, saveDataset
 
 dataset_name = 'Cornell_base'
 cornell_path = '/data'
 
-ds = Dataset(dataset_name, cornell_path)
+ds = Dataset(dataset_name, cornell_path, silence=True)
 
 ds.setInput('data/Cornell_train_query.en',
     'train',
     type='text',
     id='source_text',
     pad_on_batch=True,
-    tokenization='tokenize_none',
+    tokenization='tokenize_basic',
     build_vocabulary=True,
     fill='end',
     max_text_len=30,
@@ -25,7 +27,7 @@ ds.setInput('data/Cornell_valid_query.en',
     type='text',
     id='source_text',
     pad_on_batch=True,
-    tokenization='tokenize_none',
+    tokenization='tokenize_basic',
     fill='end',
     max_text_len=30,
     max_words=0)
@@ -35,7 +37,7 @@ ds.setInput('data/Cornell_train_reply_offset.en',
     type='text',
     id='state_below',
     required=False,
-    tokenization='tokenize_none',
+    tokenization='tokenize_basic',
     pad_on_batch=True,
     build_vocabulary='target_text',
     offset=1,
@@ -54,7 +56,7 @@ ds.setOutput('data/Cornell_train_reply.en',
     'train',
     type='text',
     id='target_text',
-    tokenization='tokenize_none',
+    tokenization='tokenize_basic',
     build_vocabulary=True,
     pad_on_batch=True,
     sample_weights=True,
@@ -67,14 +69,12 @@ ds.setOutput('data/Cornell_valid_reply.en',
     type='text',
     id='target_text',
     pad_on_batch=True,
-    tokenization='tokenize_none',
+    tokenization='tokenize_basic',
     sample_weights=True,
     max_text_len=30,
     max_words=0)
 
 #ds.merge_vocabularies(['source_text', 'target_text'])
-
-print(ds)
 
 keep_n_captions(ds, repeat=1, n=1, set_names=['val'])
 
