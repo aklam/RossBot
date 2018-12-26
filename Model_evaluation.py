b7aa5542e2b29c819985385979740fe2b0dc1b98
aklam <aklam@princeton.edu>
2018-12-26 12:37:16 -0800

    won't load model, don't know why

--- Model_evaluation.py
+++ Model_evaluation.py
@@ -6,7 +6,7 @@ from config import load_parameters
 from data_engine.prepare_data import keep_n_captions
 from keras_wrapper.cnn_model import loadModel
 from keras_wrapper.dataset import loadDataset
-
+from keras_wrapper.utils import decode_predictions_beam_search
 
 params = load_parameters()
 dataset = loadDataset('query_to_reply/Dataset_Cornell_base.pkl')
@@ -34,6 +34,8 @@ params['OUTPUT_VOCABULARY_SIZE'] = dataset.vocabulary_len[params['OUTPUTS_IDS_DA
 
 model = loadModel('trained_models/Dec_25', 2000)
 
+print("LOADED MODEL SUCCESSFULLY!!!\n\n\n")
+
 params_prediction = {'max_batch_size': 50, 'n_parallel_loaders': 8, 'predict_on_sets': ['test'], 'beam_size': 12, 'maxlen': 50, 'model_inputs': ['source_text', 'state_below'], 'model_outputs': ['target_text'], 'dataset_inputs': ['source_text', 'state_below'], 'dataset_outputs': ['target_text'], 'normalize': True, 'alpha_factor': 0.6 }
 
 predictions = model.predictBeamSearchNet(dataset, params_prediction)['test']
@@ -43,19 +45,19 @@ predictions = decode_predictions_beam_search(predictions, vocab, verbose=params[
 list2file("~/test_sampling.pred", predictions)
 
 ## see how they compare to ground truth
-from model_zoo import TranslationModel
-from keras_wrapper.extra.read_write import list2file
-from keras_wrapper.extra import evaluation
+#from model_zoo import TranslationModel
+#from keras_wrapper.extra.read_write import list2file
+#from keras_wrapper.extra import evaluation
 
-dataset.setOutput('data/Cornell_test_reply.en', 'test', type='text', id='target_text', pad_on_batch=True, tokenization='tokenize_none', sample_weights=True, max_text_len=30, max_words=0)
+#dataset.setOutput('data/Cornell_test_reply.en', 'test', type='text', id='target_text', pad_on_batch=True, tokenization='tokenize_none', sample_weights=True, max_text_len=30, max_words=0)
 
-keep_n_captions(dataset, repeat=1, n=1, set_names=['test'])
+#keep_n_captions(dataset, repeat=1, n=1, set_names=['test'])
 
-metric = 'coco'
+#metric = 'coco'
 # Apply sampling
-extra_vars = dict()
-extra_vars['tokenize_f'] = eval('dataset.' + 'tokenize_none')
-extra_vars['language'] = params['TRG_LAN']
-extra_vars['test'] = dict()
-extra_vars['test']['references'] = dataset.extra_variables['test']['target_text']
-metrics = evaluation.select[metric](pred_list=predictions, verbose=1, extra_vars=extra_vars, split='test')
+#extra_vars = dict()
+#extra_vars['tokenize_f'] = eval('dataset.' + 'tokenize_none')
+#extra_vars['language'] = params['TRG_LAN']
+#extra_vars['test'] = dict()
+#extra_vars['test']['references'] = dataset.extra_variables['test']['target_text']
+#metrics = evaluation.select[metric](pred_list=predictions, verbose=1, extra_vars=extra_vars, split='test')
