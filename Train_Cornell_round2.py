2fefc8efdc2682d818e270450e9f7ed67c689c71
aklam <aklam@princeton.edu>
2018-12-27 14:29:45 -0800

    v2 attempt at reloading weights

--- Train_Cornell_round2.py
+++ Train_Cornell_round2.py
@@ -12,7 +12,41 @@ from model_zoo import TranslationModel
 
 
 Cornell_Rd2 = loadDataset('query_to_reply/Dataset_Cornell_Rd2.pkl')
-model = loadModel('trained_models/Dec_26', 1)
+
+params = load_parameters()
+params['INPUT_VOCABULARY_SIZE'] = Cornell_Rd2.vocabulary_len[params['INPUTS_IDS_DATASET'][0]]
+params['OUTPUT_VOCABULARY_SIZE'] = Cornell_Rd2.vocabulary_len[params['OUTPUTS_IDS_DATASET'][0]]
+
+params['SOURCE_TEXT_EMBEDDING_SIZE'] = 300
+params['TARGET_TEXT_EMBEDDING_SIZE'] = 300
+
+params['SRC_PRETRAINED_VECTORS'] = '../Google_w2v.npy'
+params['TRG_PRETRAINED_VECTORS'] = '../Google_w2v.npy'
+
+
+#Model parameters
+params['ENCODER_RNN_TYPE'] = 'GRU'
+params['DECODER_RNN_TYPE'] = 'GRU'
+
+#params['N_LAYERS_ENCODER'] = 2
+#params['N_LAYERS_DECODER'] = 2
+params['ENCODER_HIDDEN_SIZE'] = 512
+params['DECODER_HIDDEN_SIZE'] = 512
+params['MODEL_SIZE'] = 512
+params['SRC_PRETRAINED_VECTORS_TRAINABLE'] = False
+params['TRG_PRETRAINED_VECTORS_TRAINABLE'] = False 
+
+#params['SKIP_VECTORS_HIDDEN_SIZE'] = 512
+params['ATTENTION_SIZE'] = 512
+
+nmt_model = TranslationModel(params, 
+	model_type='GroundHogModel',
+	model_name='Dec_27',
+	weights_path='trained_models/Dec_26/epoch_1_init.h5'
+	vocabularies=Cornell_Rd2.vocabulary,
+	store_path='trained_models/Dec_26/',
+	verbose=True)
+
 
 training_params = {'n_epochs': 1, 'batch_size': 80,'maxlen': 30, 'epochs_for_save': 5, 'verbose': 1, 'eval_on_sets': [], 'reload_epoch': 1, 'epoch_offset': 1}
 
