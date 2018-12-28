336acab9d223c81a9db57f916c932af2d9516063
aklam <aklam@princeton.edu>
2018-12-27 18:39:08 -0800

    Data mapping

--- Train_Cornell_round2.py
+++ Train_Cornell_round2.py
@@ -43,8 +43,6 @@ params['RELOAD'] = 1
 params['RELOAD_EPOCH'] = True
 params['REBUILD_DATASET'] = False
 
-ds = loadDataset('query_to_reply/Dataset_Cornell_base.pkl')
-
 nmt_model = TranslationModel(params, 
 	model_type='GroundHogModel',
     weights_path='trained_models/Dec_27/epoch_1_init.h5',
@@ -52,6 +50,20 @@ nmt_model = TranslationModel(params,
 	vocabularies=Cornell_Rd2.vocabulary,
 	store_path='trained_models/Dec_27_v1/',
 	verbose=True)
+
+inputMapping = dict()
+for i, id_in in enumerate(params['INPUTS_IDS_DATASET']):
+    pos_source = Cornell_Rd2.ids_inputs.index(id_in)
+    id_dest = nmt_model.ids_inputs[i]
+    inputMapping[id_dest] = pos_source
+
+nmt_model.setInputsMapping(inputMapping)
+outputMapping = dict()
+for i, id_out in enumerate(params['OUTPUTS_IDS_DATASET']):
+    pos_target = Cornell_Rd2.ids_outputs.index(id_out)
+    id_dest = nmt_model.ids_outputs[i]
+    outputMapping[id_dest] = pos_target
+nmt_model.setOutputsMapping(outputMapping)
 
 
 training_params = {'n_epochs': 10, 'batch_size': 20,'maxlen': 30, 'epochs_for_save': 5, 'verbose': 1, 'eval_on_sets': [], 'reload_epoch': 1, 'epoch_offset': 1}
