b0f0a76aad42d44b98ca257dfb26261dba37f610
aklam <aklam@princeton.edu>
2018-12-28 21:22:57 -0800

    Removed vocabulary stuff

--- Train_Cornell_round2.py
+++ Train_Cornell_round2.py
@@ -11,7 +11,8 @@ from keras_wrapper.utils import decode_predictions_beam_search
 from model_zoo import TranslationModel
 
 
-Cornell_Rd2 = loadDataset('query_to_reply/Dataset_Cornell.pkl')
+
+ds = loadDataset('query_to_reply/Dataset_Cornell.pkl')
 
 params = load_parameters()
 
@@ -50,11 +51,10 @@ nmt_model = TranslationModel(params,
 	model_type='GroundHogModel',
     weights_path='trained_models/Dec_27/epoch_1_init.h5',
 	model_name='Dec_27',
-	vocabularies=Cornell_Rd2.vocabulary,
 	store_path='trained_models/Dec_27_v1/',
 	verbose=True)
 
 
 training_params = {'n_epochs': 1, 'batch_size': 80,'maxlen': 30, 'epochs_for_save': 5, 'verbose': 1, 'eval_on_sets': [], 'reload_epoch': 1, 'epoch_offset': 1}
 
-model.trainNet(Cornell_Rd2, training_params)
+model.trainNet(ds, training_params)
