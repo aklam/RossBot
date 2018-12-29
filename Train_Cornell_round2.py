190933a7f2c780bc5a3dde12d23209ffa2658533
aklam <aklam@princeton.edu>
2018-12-29 10:53:44 -0800

    typo

--- Train_Cornell_round2.py
+++ Train_Cornell_round2.py
@@ -55,7 +55,7 @@ params['TEXT_FILES'] = {'train': 'Cornell_train_2.', 'val': 'Cornell_valid_2.'}
 ds = update_dataset_from_file(ds=ds, 
 	input_text_filename='data/Cornell_train_2.query',
 	params=params,
-	splits=['train','val']
+	splits=['train','val'],
 	output_text_filename='data/Cornell_train_2.reply',
 	compute_state_below=False,
 	recompute_references=False)
