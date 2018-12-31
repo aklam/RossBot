b8b83bf1d78055553b0684d21508c51092771796
aklam <aklam@princeton.edu>
2018-12-31 12:24:13 -0800

    The commit before this one trains successfully, adding in more complex features

--- Resume_training.py
+++ Resume_training.py
@@ -16,33 +16,29 @@ ds = loadDataset('query_to_reply/Dataset_Cornell_base.pkl')
 
 params = load_parameters()
 
-# For the model params, INPUT_VOCABULARY_SIZE and OUTPUT_VOCABULARY_SIZE should I set that equal to the size of the new or the old dataset?
+params['SOURCE_TEXT_EMBEDDING_SIZE'] = 300
+params['TARGET_TEXT_EMBEDDING_SIZE'] = 300
 
-# Should the vocabularies argument in TranslationModel be set to the new dataset vocabulary or the old dataset vocabulary?
+params['SRC_PRETRAINED_VECTORS'] = '../Google_w2v.npy'
+params['TRG_PRETRAINED_VECTORS'] = '../Google_w2v.npy'
 
-#params['SOURCE_TEXT_EMBEDDING_SIZE'] = 300
-#params['TARGET_TEXT_EMBEDDING_SIZE'] = 300
-#
-#params['SRC_PRETRAINED_VECTORS'] = '../Google_w2v.npy'
-#params['TRG_PRETRAINED_VECTORS'] = '../Google_w2v.npy'
-#
-#
-##Model parameters
-#params['ENCODER_RNN_TYPE'] = 'GRU'
-#params['DECODER_RNN_TYPE'] = 'GRU'
-#
-#params['N_LAYERS_ENCODER'] = 2
-#params['N_LAYERS_DECODER'] = 2
-#params['ENCODER_HIDDEN_SIZE'] = 512
-#params['DECODER_HIDDEN_SIZE'] = 512
-#params['MODEL_SIZE'] = 512
-#params['SRC_PRETRAINED_VECTORS_TRAINABLE'] = False
-#params['TRG_PRETRAINED_VECTORS_TRAINABLE'] = False 
-#
-##params['SKIP_VECTORS_HIDDEN_SIZE'] = 512
-#params['ATTENTION_SIZE'] = 512
 
-params['RELOAD'] = 1
+#Model parameters
+params['ENCODER_RNN_TYPE'] = 'GRU'
+params['DECODER_RNN_TYPE'] = 'GRU'
+
+params['N_LAYERS_ENCODER'] = 2
+params['N_LAYERS_DECODER'] = 2
+params['ENCODER_HIDDEN_SIZE'] = 512
+params['DECODER_HIDDEN_SIZE'] = 512
+params['MODEL_SIZE'] = 512
+params['SRC_PRETRAINED_VECTORS_TRAINABLE'] = False
+params['TRG_PRETRAINED_VECTORS_TRAINABLE'] = False 
+
+#params['SKIP_VECTORS_HIDDEN_SIZE'] = 512
+params['ATTENTION_SIZE'] = 512
+
+params['RELOAD'] = 24
 params['RELOAD_EPOCH'] = True
 params['REBUILD_DATASET'] = False
 params['DATA_ROOT_PATH'] = 'data/'
@@ -52,14 +48,6 @@ params['SRC_LAN'] = 'query'
 params['TRG_LAN'] = 'reply'
 params['TEXT_FILES'] = {'train': 'Cornell_train_2.', 'val': 'Cornell_valid_2.'}
 params['TOKENIZATION_METHOD'] = 'tokenize_basic'
-
-#ds_2 = update_dataset_from_file(ds=ds, 
-#    input_text_filename='data/Cornell_train_2.query',
-#    params=params,
-#    splits=['train'],
-#    output_text_filename='data/Cornell_train_2.reply',
-#    compute_state_below=True,
-#    recompute_references=False)
 
 ds.setInput('data/Cornell_train_2.query',
     'train',
@@ -105,10 +93,10 @@ print(ds)
 
 nmt_model = TranslationModel(params, 
     model_type='GroundHogModel',
-    weights_path='trained_models/Vanilla_model_resume_test/epoch_1_init.h5',
-    model_name='Vanilla_model_resumed',
+    weights_path='trained_models/Vanilla_model_resume_test/epoch_24_init.h5',
+    model_name='2LayerGRU_resume_test',
     vocabularies=ds.vocabulary,
-    store_path='trained_models/Vanilla_model_resumed/',
+    store_path='trained_models/2LayerGRU_resume_test/',
     verbose=True)
 
 inputMapping = dict()
@@ -125,7 +113,7 @@ for i, id_out in enumerate(params['OUTPUTS_IDS_DATASET']):
     outputMapping[id_dest] = pos_target
 nmt_model.setOutputsMapping(outputMapping)
 
-training_params = {'n_epochs': 3, 'batch_size': 20,'maxlen': 30, 'epochs_for_save': 1, 'verbose': 1, 'eval_on_sets': [], 'reload_epoch': 1, 'epoch_offset': 1}
+training_params = {'n_epochs': 26, 'batch_size': 20,'maxlen': 30, 'epochs_for_save': 1, 'verbose': 1, 'eval_on_sets': [], 'reload_epoch': 24, 'epoch_offset': 24}
 
 print(ds)
 
