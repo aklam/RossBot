5047c9838e9e98edc3b0f51d0eb8bc4a13ffd627
aklam <aklam@princeton.edu>
2019-01-02 19:34:21 -0800

    Train model vectors a second time

--- Resume_training.py
+++ Resume_training.py
@@ -38,7 +38,7 @@ params['MODEL_SIZE'] = 512
 #params['SKIP_VECTORS_HIDDEN_SIZE'] = 512
 params['ATTENTION_SIZE'] = 512
 
-params['RELOAD'] = 8
+params['RELOAD'] = 10
 params['RELOAD_EPOCH'] = True
 params['REBUILD_DATASET'] = False
 params['DATA_ROOT_PATH'] = 'data/'
@@ -49,61 +49,61 @@ params['DATA_ROOT_PATH'] = 'data/'
 #params['TEXT_FILES'] = {'train': 'Cornell_train_2.', 'val': 'Cornell_valid_2.'}
 params['TOKENIZATION_METHOD'] = 'tokenize_basic'
 
-ds.setInput('data/Ross_train.query',
-    'train',
-    type='text',
-    id='source_text',
-    tokenization='tokenize_basic',
-    pad_on_batch=True,
-    fill='end',
-    max_text_len=30,
-    min_occ=0,
-    overwrite_split=True)
-
-ds.setInput('data/Ross_train.reply',
-    'train',
-    type='text',
-    id='state_below',
-    required=False,
-    tokenization='tokenize_basic',
-    pad_on_batch=True,
-    offset=1,
-    fill='end',
-    max_text_len=30,
-    overwrite_split=True)
-
-ds.setOutput('data/Ross_train.reply',
-    'train',
-    type='text',
-    id='target_text',
-    tokenization='tokenize_basic',
-    pad_on_batch=True,
-    sample_weights=True,
-    max_text_len=30,
-    min_occ=0,
-    overwrite_split=True)
-
-ds.setInput('data/Ross_valid.query',
-    'val',
-    type='text',
-    id='source_text',
-    tokenization='tokenize_basic',
-    pad_on_batch=True,
-    fill='end',
-    max_text_len=30,
-    min_occ=0,
-    overwrite_split=True)
-
-ds.setOutput('data/Ross_valid.reply',
-    'val',
-    type='text',
-    id='target_text',
-    tokenization='tokenize_basic',
-    pad_on_batch=True,
-    sample_weights=True,
-    max_text_len=30,
-    min_occ=0,
-    overwrite_split=True)
+#ds.setInput('data/Ross_train.query',
+#    'train',
+#    type='text',
+#    id='source_text',
+#    tokenization='tokenize_basic',
+#    pad_on_batch=True,
+#    fill='end',
+#    max_text_len=30,
+#    min_occ=0,
+#    overwrite_split=True)
+#
+#ds.setInput('data/Ross_train.reply',
+#    'train',
+#    type='text',
+#    id='state_below',
+#    required=False,
+#    tokenization='tokenize_basic',
+#    pad_on_batch=True,
+#    offset=1,
+#    fill='end',
+#    max_text_len=30,
+#    overwrite_split=True)
+#
+#ds.setOutput('data/Ross_train.reply',
+#    'train',
+#    type='text',
+#    id='target_text',
+#    tokenization='tokenize_basic',
+#    pad_on_batch=True,
+#    sample_weights=True,
+#    max_text_len=30,
+#    min_occ=0,
+#    overwrite_split=True)
+#
+#ds.setInput('data/Ross_valid.query',
+#    'val',
+#    type='text',
+#    id='source_text',
+#    tokenization='tokenize_basic',
+#    pad_on_batch=True,
+#    fill='end',
+#    max_text_len=30,
+#    min_occ=0,
+#    overwrite_split=True)
+#
+#ds.setOutput('data/Ross_valid.reply',
+#    'val',
+#    type='text',
+#    id='target_text',
+#    tokenization='tokenize_basic',
+#    pad_on_batch=True,
+#    sample_weights=True,
+#    max_text_len=30,
+#    min_occ=0,
+#    overwrite_split=True)
 
 params['INPUT_VOCABULARY_SIZE'] = ds.vocabulary_len[params['INPUTS_IDS_DATASET'][0]]
 params['OUTPUT_VOCABULARY_SIZE'] = ds.vocabulary_len[params['OUTPUTS_IDS_DATASET'][0]]
@@ -112,10 +112,10 @@ print(ds)
 
 nmt_model = TranslationModel(params, 
     model_type='GroundHogModel',
-    weights_path='trained_models/Full_model_trainable_w2v/epoch_8_init.h5',
-    model_name='Ross_train_w2v_start8',
+    weights_path='trained_models/Full_model/epoch_10_init.h5',
+    model_name='Full_model_2rnd_trainVecs',
     vocabularies=ds.vocabulary,
-    store_path='trained_models/Ross_train_w2v_start8/',
+    store_path='trained_models/Full_model_2rnd_trainVecs/',
     verbose=True)
 
 inputMapping = dict()
@@ -132,7 +132,7 @@ for i, id_out in enumerate(params['OUTPUTS_IDS_DATASET']):
     outputMapping[id_dest] = pos_target
 nmt_model.setOutputsMapping(outputMapping)
 
-training_params = {'n_epochs': 20, 'batch_size': 20,'maxlen': 30, 'epochs_for_save': 2, 'verbose': 2, 'eval_on_sets': [], 'reload_epoch': 8, 'epoch_offset': 8}
+training_params = {'n_epochs': 20, 'batch_size': 20,'maxlen': 30, 'epochs_for_save': 2, 'verbose': 2, 'eval_on_sets': [], 'reload_epoch': 10, 'epoch_offset': 10}
 
 print(ds)
 
