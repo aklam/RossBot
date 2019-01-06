bbd8a602ffb36253092d2a46839d2731932c2383
aklam <aklam@princeton.edu>
2019-01-05 20:54:47 -0500

    Controls 1-3

--- Control_Rnd2_w2v.py
+++ Control_Rnd2_w2v.py
@@ -32,8 +32,8 @@ params['N_LAYERS_DECODER'] = 2
 params['ENCODER_HIDDEN_SIZE'] = 512
 params['DECODER_HIDDEN_SIZE'] = 512
 params['MODEL_SIZE'] = 512
-params['SRC_PRETRAINED_VECTORS_TRAINABLE'] = False
-params['TRG_PRETRAINED_VECTORS_TRAINABLE'] = False 
+params['SRC_PRETRAINED_VECTORS_TRAINABLE'] = True
+params['TRG_PRETRAINED_VECTORS_TRAINABLE'] = True 
 
 #params['SKIP_VECTORS_HIDDEN_SIZE'] = 512
 params['ATTENTION_SIZE'] = 512
@@ -109,10 +109,10 @@ print(ds)
 
 nmt_model = TranslationModel(params, 
     model_type='GroundHogModel',
-    weights_path='trained_models/512_Base_Epoch11/epoch_11_init.h5',
-    model_name='Control_M1',
+    weights_path='trained_models/512_Trained_w2v_Base/epoch_11_init.h5',
+    model_name='Control_M3',
     vocabularies=ds.vocabulary,
-    store_path='trained_models/Control_M1/',
+    store_path='trained_models/Control_M3/',
     verbose=True)
 
 inputMapping = dict()
@@ -129,6 +129,6 @@ for i, id_out in enumerate(params['OUTPUTS_IDS_DATASET']):
     outputMapping[id_dest] = pos_target
 nmt_model.setOutputsMapping(outputMapping)
 
-training_params = {'n_epochs': 18, 'batch_size': 20,'maxlen': 30, 'epochs_for_save': 1, 'verbose': 1, 'eval_on_sets': [], 'reload_epoch': 11, 'epoch_offset': 11}
+training_params = {'n_epochs': 22, 'batch_size': 20,'maxlen': 30, 'epochs_for_save': 1, 'verbose': 1, 'eval_on_sets': [], 'reload_epoch': 11, 'epoch_offset': 11}
 
 nmt_model.trainNet(ds, training_params)
