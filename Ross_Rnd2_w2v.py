5daaff7c0605383045714ee51d065a79880e00d2
aklam <aklam@princeton.edu>
2019-01-05 21:05:04 -0500

    Ross models 4-6

--- Ross_Rnd2_w2v.py
+++ Ross_Rnd2_w2v.py
@@ -29,14 +29,14 @@ params['DECODER_RNN_TYPE'] = 'GRU'
 
 params['N_LAYERS_ENCODER'] = 2
 params['N_LAYERS_DECODER'] = 2
-params['ENCODER_HIDDEN_SIZE'] = 512
-params['DECODER_HIDDEN_SIZE'] = 512
-params['MODEL_SIZE'] = 512
+params['ENCODER_HIDDEN_SIZE'] = 1024
+params['DECODER_HIDDEN_SIZE'] = 1024
+params['MODEL_SIZE'] = 1024
 params['SRC_PRETRAINED_VECTORS_TRAINABLE'] = True
 params['TRG_PRETRAINED_VECTORS_TRAINABLE'] = True 
 
 #params['SKIP_VECTORS_HIDDEN_SIZE'] = 512
-params['ATTENTION_SIZE'] = 512
+params['ATTENTION_SIZE'] = 1024
 
 params['RELOAD'] = 11
 params['RELOAD_EPOCH'] = True
@@ -109,10 +109,10 @@ print(ds)
 
 nmt_model = TranslationModel(params, 
     model_type='GroundHogModel',
-    weights_path='trained_models/512_Trained_w2v_Base/epoch_11_init.h5',
-    model_name='Ross_M3',
+    weights_path='trained_models/1024_Trained_w2v_Base/epoch_11_init.h5',
+    model_name='Ross_M6',
     vocabularies=ds.vocabulary,
-    store_path='trained_models/Ross_M3/',
+    store_path='trained_models/Ross_M6/',
     verbose=True)
 
 inputMapping = dict()
