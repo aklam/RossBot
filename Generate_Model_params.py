bda9f458f46bcbdba061b43521d370fec8a1dc88
aklam <aklam@princeton.edu>
2019-01-07 02:18:57 -0500

    hack loss together

--- Generate_Model_params.py
+++ Generate_Model_params.py
@@ -1,42 +1,133 @@
 import sys
-import os
 sys.path.append('../nmt-keras')
 sys.path.append('../nmt-keras/nmt_keras')
 
 import utils 
 from config import load_parameters
-from keras_wrapper.extra.read_write import dict2pkl
-
-if not os.path.exists("model_params"):
-    os.makedirs("model_params")
-
-def make_params(model_name, model_size):
-	params = load_parameters()
-	params['INPUT_VOCABULARY_SIZE'] = 30000
-	params['OUTPUT_VOCABULARY_SIZE'] = 30000
-
-	params['SOURCE_TEXT_EMBEDDING_SIZE'] = 300
-	params['TARGET_TEXT_EMBEDDING_SIZE'] = 300
-
-	params['SRC_PRETRAINED_VECTORS'] = '../Google_w2v.npy'
-	params['TRG_PRETRAINED_VECTORS'] = '../Google_w2v.npy'
-
-	params['ENCODER_RNN_TYPE'] = 'GRU'
-	params['DECODER_RNN_TYPE'] = 'GRU'
-
-	params['N_LAYERS_ENCODER'] = 2
-	params['N_LAYERS_DECODER'] = 2
-	params['ENCODER_HIDDEN_SIZE'] = model_size
-	params['DECODER_HIDDEN_SIZE'] = model_size
-	params['MODEL_SIZE'] = model_size
-	params['ATTENTION_SIZE'] = model_size
-
-	params['SRC_PRETRAINED_VECTORS_TRAINABLE'] = True
-	params['TRG_PRETRAINED_VECTORS_TRAINABLE'] = True 
-
-	params['RELOAD'] = 22
-	params['MODEL_NAME'] = model_name
-	dict2pkl(params, "model_params/"+model_name)
+from data_engine.prepare_data import keep_n_captions, update_dataset_from_file
+from keras_wrapper.cnn_model import loadModel
+from keras_wrapper.dataset import loadDataset
+from keras_wrapper.utils import decode_predictions_beam_search
+from model_zoo import TranslationModel
 
 
-make_params("Ross_M3", 512)
+#Dataset_Cornell_base is the large dataset as a keras_wrapper.dataset Dataset instance
+ds = loadDataset('query_to_reply/Dataset_Cornell_base.pkl')
+
+params = load_parameters()
+
+params['SOURCE_TEXT_EMBEDDING_SIZE'] = 300
+params['TARGET_TEXT_EMBEDDING_SIZE'] = 300
+
+params['SRC_PRETRAINED_VECTORS'] = '../Google_w2v.npy'
+params['TRG_PRETRAINED_VECTORS'] = '../Google_w2v.npy'
+
+
+#Model parameters
+params['ENCODER_RNN_TYPE'] = 'GRU'
+params['DECODER_RNN_TYPE'] = 'GRU'
+
+params['N_LAYERS_ENCODER'] = 2
+params['N_LAYERS_DECODER'] = 2
+params['ENCODER_HIDDEN_SIZE'] = 512
+params['DECODER_HIDDEN_SIZE'] = 512
+params['MODEL_SIZE'] = 512
+params['SRC_PRETRAINED_VECTORS_TRAINABLE'] = False
+params['TRG_PRETRAINED_VECTORS_TRAINABLE'] = False 
+
+params['ATTENTION_SIZE'] = 512
+
+params['RELOAD'] = 36
+params['RELOAD_EPOCH'] = True
+params['REBUILD_DATASET'] = False
+params['DATA_ROOT_PATH'] = 'data/'
+
+
+params['TOKENIZATION_METHOD'] = 'tokenize_basic'
+
+ds.setInput('data/dumby.query',
+    'train',
+    type='text',
+    id='source_text',
+    tokenization='tokenize_basic',
+    pad_on_batch=True,
+    fill='end',
+    max_text_len=30,
+    min_occ=0,
+    overwrite_split=True)
+
+ds.setInput('data/dumby.reply',
+    'train',
+    type='text',
+    id='state_below',
+    required=False,
+    tokenization='tokenize_basic',
+    pad_on_batch=True,
+    offset=1,
+    fill='end',
+    max_text_len=30,
+    overwrite_split=True)
+
+ds.setOutput('data/dumby.reply',
+    'train',
+    type='text',
+    id='target_text',
+    tokenization='tokenize_basic',
+    pad_on_batch=True,
+    sample_weights=True,
+    max_text_len=30,
+    min_occ=0,
+    overwrite_split=True)
+
+ds.setInput('data/Ross_test.query',
+    'val',
+    type='text',
+    id='source_text',
+    tokenization='tokenize_basic',
+    pad_on_batch=True,
+    fill='end',
+    max_text_len=30,
+    min_occ=0,
+    overwrite_split=True)
+
+ds.setOutput('data/Ross_test.reply',
+    'val',
+    type='text',
+    id='target_text',
+    tokenization='tokenize_basic',
+    pad_on_batch=True,
+    sample_weights=True,
+    max_text_len=30,
+    min_occ=0,
+    overwrite_split=True)
+
+params['INPUT_VOCABULARY_SIZE'] = ds.vocabulary_len[params['INPUTS_IDS_DATASET'][0]]
+params['OUTPUT_VOCABULARY_SIZE'] = ds.vocabulary_len[params['OUTPUTS_IDS_DATASET'][0]]
+
+print(ds)
+
+nmt_model = TranslationModel(params, 
+    model_type='GroundHogModel',
+    weights_path='trained_models/Ross_M3/epoch_36_init.h5',
+    model_name='tmp',
+    vocabularies=ds.vocabulary,
+    store_path='trained_models/tmp/',
+    verbose=True)
+
+inputMapping = dict()
+for i, id_in in enumerate(params['INPUTS_IDS_DATASET']):
+    pos_source = ds.ids_inputs.index(id_in)
+    id_dest = nmt_model.ids_inputs[i]
+    inputMapping[id_dest] = pos_source
+
+nmt_model.setInputsMapping(inputMapping)
+outputMapping = dict()
+for i, id_out in enumerate(params['OUTPUTS_IDS_DATASET']):
+    pos_target = ds.ids_outputs.index(id_out)
+    id_dest = nmt_model.ids_outputs[i]
+    outputMapping[id_dest] = pos_target
+nmt_model.setOutputsMapping(outputMapping)
+
+training_params = {'n_epochs': 37, 'batch_size': 1,'maxlen': 30, 'epochs_for_save': 5, 'verbose': 1, 'eval_on_sets': [], 'reload_epoch': 36, 'epoch_offset': 36}
+
+nmt_model.trainNet(ds, training_params)
